c9b18344f1e3400163b4acc42c1d8de4acc21772
wuchen <chenorange12@gmail.com>
2021-10-21 01:44:16 +0800

    feat: project init

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+ const fabric= require('@umijs/fabric/');
+
+module.exports = {
+  extends: [...fabric.eslint.extends,'next/core-web-vitals'],
+
+  // in antd-design-pro
+  globals: {},
+
+  rules: {
+    // your rules
+  },
+};
