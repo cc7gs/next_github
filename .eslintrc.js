12f99ca011ba5857c4803ad981e68188d1103d84
wuchen <chenorange12@gmail.com>
2021-10-21 22:47:12 +0800

    chore: update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
  const fabric= require('@umijs/fabric/');
 
 module.exports = {
-  extends: [...fabric.eslint.extends,'next/core-web-vitals'],
+  ...fabric.default,
+  extends: [...fabric.default.extends,'next/core-web-vitals'],
 
-  // in antd-design-pro
   globals: {},
 
   rules: {
