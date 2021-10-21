ee461424e4dc74b7b58a9add23e994b9f779d27b
wuchen <chenorange12@gmail.com>
2021-10-22 00:33:08 +0800

    chore: rewrite vs redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  basePath: '/home',
+  async rewrites() {
+    return [
+      {
+        source: '/about-us',
+        destination: '/about',
+      },
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: '/team',
+        destination: '/about',
+        permanent: false,
+      },
+    ];
+  },
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,11 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import { useRouter } from 'next/router';
 
 const Home: NextPage = () => {
+  const { basePath } = useRouter();
+  console.log(basePath);
   return (
     <div className={styles.container}>
       <Head>
